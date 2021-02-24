85b9c1fa2b6415dbe572eb2b3829f5da46233b30
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 17:08:30 -0300

    Seleção Composta

--- SQL.sql
+++ SQL.sql
@@ -1,17 +1,6 @@
 USE sucos;
 
-SELECT * FROM tbproduto;
-
-SELECT * FROM tbproduto WHERE preco_lista BETWEEN 16.007 AND 16.009;
-SELECT * FROM tbproduto WHERE preco_lista >= 16.007;
-SELECT * FROM tbproduto WHERE preco_lista <= 16.009;
-SELECT * FROM tbproduto WHERE preco_lista >= 16.007 AND preco_lista <= 16.009;
-
-SELECT * FROM tbcliente;
-
-SELECT * FROM tbcliente WHERE idade >= 18 AND idade <= 22;
-SELECT * FROM tbcliente WHERE idade >= 18 AND idade <= 22 AND sexo='M';
-SELECT * FROM tbcliente WHERE cidade='Rio de Janeiro' OR bairro='Jardins';
-SELECT * FROM tbcliente WHERE (idade >= 18 AND idade <=22 AND sexo='M') OR (cidade='Rio de Janeiro' OR bairro='Jardins');
+SELECT * FROM tabela_de_vendedores;
+SELECT * FROM tabela_de_vendedores WHERE year(data_admissao) < 2016 AND de_ferias=1;
 
 
