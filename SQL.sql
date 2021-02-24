15200db86681ba93b2ffe8d6086ab18cb6e0351b
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:07:03 -0300

    Visualizando os registros da tabela de produtos

--- SQL.sql
+++ SQL.sql
@@ -1,8 +1,3 @@
 USE sucos;
 
-INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
-VALUES('1037797', 'Clean - 2 Litros - Laranja', 'PET', '2 Litros', 'Laranja', 16.01);
-INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
-VALUES('1000889', 'Sabor da Montanha - 700 ml - Uva', 'Garrafa', '700ml', 'Uva', 6.31);
-INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
-VALUES('1004327', 'Videira do Campo - 1,5 Litros - Melancia', 'PET', '1,5 Litros', 'Melancia', 19.51); 
+SELECT * FROM tbproduto;
