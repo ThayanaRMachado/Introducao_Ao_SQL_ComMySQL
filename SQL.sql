73b3473291d9965d5179f9c6590732b11af26f5a
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:01:46 -0300

    Apagando tabelas

--- SQL.sql
+++ SQL.sql
@@ -1,7 +1,9 @@
 USE sucos;
 
-CREATE TABLE tabela_de_vendedores(
+CREATE TABLE tabela_de_vendedores1(
 	matricula VARCHAR(5),
     nome VARCHAR(100),
     percentual_comissao FLOAT
 ); 
+
+DROP TABLE tabela_de_vendedores1; 
