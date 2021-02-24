cd4322ee4d0251392c2d338f938882984fb49c44
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:27:33 -0300

    Filtrando registros

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,9 @@
 USE sucos;
 
-SELECT  nome, matricula FROM tabela_de_vendedores;
+SELECT * FROM tbproduto WHERE PRODUTO = '544931';
+
+SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
+
+SELECT * FROM tbproduto WHERE SABOR = 'Cítricos';
+
+UPDATE tbproduto SET SABOR = 'Cítricos' WHERE SABOR = 'Limão';
