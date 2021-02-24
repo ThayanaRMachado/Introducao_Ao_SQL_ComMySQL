5af10a21cc5507af2fbda8767d040fb138cd04df
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:10:49 -0300

    Alterando informações sobre os vendedores

--- SQL.sql
+++ SQL.sql
@@ -1,10 +1,4 @@
 USE sucos;
 
-INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
-VALUES('544931', 'Frescor do Verão - 350 ml - Limão', 'PET', '350 ML', 'Limão', 3.20);
-INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
-VALUES('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ML', 'Manga', 5.18);
-
-UPDATE tbproduto SET embalagem='Garrafa' WHERE PRODUTO='1078680';
-
-SELECT * FROM tbproduto; 
+UPDATE TABELA_DE_VENDEDORES SET PERCENTUAL_COMISSAO = 0.11 WHERE MATRICULA = '00236';
+UPDATE TABELA_DE_VENDEDORES SET NOME = 'José Geraldo da Fonseca Junior' WHERE MATRICULA = '00233';
