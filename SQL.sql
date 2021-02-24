dc5d536a32b4e0c8189a0911751f1cd49dedc982
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:06:01 -0300

    Inserindo vários registros na tabela de produtos

--- SQL.sql
+++ SQL.sql
@@ -1,4 +1,8 @@
 USE sucos;
 
-INSERT INTO tabela_de_vendedores(matricula, nome, percentual_comissao)
-VALUES('00233', 'João Geraldo da Fonseca', 0.10); 
+INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
+VALUES('1037797', 'Clean - 2 Litros - Laranja', 'PET', '2 Litros', 'Laranja', 16.01);
+INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
+VALUES('1000889', 'Sabor da Montanha - 700 ml - Uva', 'Garrafa', '700ml', 'Uva', 6.31);
+INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
+VALUES('1004327', 'Videira do Campo - 1,5 Litros - Melancia', 'PET', '1,5 Litros', 'Melancia', 19.51); 
