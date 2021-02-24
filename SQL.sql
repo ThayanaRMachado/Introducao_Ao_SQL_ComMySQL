11ba5c4e8111317644a76590e776fdc8945408ba
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:39:09 -0300

    Filtrando usando maior, menor e diferente

--- SQL.sql
+++ SQL.sql
@@ -1,5 +1,27 @@
 USE sucos;
 
-SELECT * FROM tabela_de_vendedores;
+SELECT * FROM tbcliente;
 
-SELECT * FROM tabela_de_vendedores WHERE nome='Cláudia Morais';
+SELECT * FROM tbcliente WHERE idade=22;
+
+SELECT * FROM tbcliente WHERE idade>22;
+
+SELECT * FROM tbcliente WHERE idade<22;
+
+SELECT * FROM tbcliente WHERE idade <= 22;
+
+SELECT * FROM tbcliente WHERE idade <> 22;
+
+SELECT * FROM tbcliente WHERE nome>= 'Fernando Cavalcante';
+
+SELECT * FROM tbcliente WHERE nome <> 'Fernando Cavalcante';
+
+SELECT * FROM tbproduto;
+
+SELECT * FROM tbproduto WHERE preco_lista> 16.008;
+
+SELECT * FROM tbproduto WHERE preco_lista < 16.008;
+
+SELECT * FROM tbproduto WHERE preco_lista <> 16.008;
+
+SELECT * FROM tbproduto WHERE preco_lista BETWEEN 16.007 AND 16.009;
