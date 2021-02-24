7ac2152f15e3d262c2f3e19adc6d446c52fff400
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:03:23 -0300

    Criando a tabela de Produtos e inserindo dados

--- SQL.sql
+++ SQL.sql
@@ -1,9 +1,15 @@
 USE sucos;
 
-CREATE TABLE tabela_de_vendedores1(
-	matricula VARCHAR(5),
-    nome VARCHAR(100),
-    percentual_comissao FLOAT
-); 
+CREATE TABLE tbProduto(
+	produto VARCHAR(20),
+    nome VARCHAR(150),
+    embalagem VARCHAR(50),
+    tamanho VARCHAR(50),
+    sabor VARCHAR(50),
+    preco_lista FLOAT
+);
 
-DROP TABLE tabela_de_vendedores1; 
+INSERT INTO tbproduto (
+produto,  nome, embalagem, tamanho, sabor, preco_lista)
+VALUES ('1040107', 'Light - 350 ml - Melancia',
+'Lata', '350 ml', 'Melancia', 4.56); 
