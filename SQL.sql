facf8ac1acee2f37bdf325f4c5be7960062e37fe
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:09:08 -0300

    Alterando registros

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,10 @@
 USE sucos;
 
-INSERT INTO tabela_de_vendedores(matricula, nome, percentual_comissao)
-VALUES('00235', 'Márcio Almeida Silva', 0.08 );
-INSERT INTO tabela_de_vendedores(matricula, nome, percentual_comissao)
-VALUES('00236', 'Cláudia Morais', 0.08 );
+INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
+VALUES('544931', 'Frescor do Verão - 350 ml - Limão', 'PET', '350 ML', 'Limão', 3.20);
+INSERT INTO tbproduto(produto, nome, embalagem, tamanho, sabor, preco_lista)
+VALUES('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ML', 'Manga', 5.18);
+
+UPDATE tbproduto SET embalagem='Garrafa' WHERE PRODUTO='1078680';
+
+SELECT * FROM tbproduto; 
