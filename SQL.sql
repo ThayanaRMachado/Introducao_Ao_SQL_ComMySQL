e3e4c908fc3152ed9a23267cf14eeccf9f3a795d
ThayanaRMachado <thayanarm@hotmail.com>
2021-02-24 16:15:34 -0300

    Incluindo a chave primária

--- SQL.sql
+++ SQL.sql
@@ -1,5 +1,13 @@
 USE sucos;
 
-SELECT * FROM tabela_de_vendedores;
+ALTER TABLE tbproduto ADD PRIMARY KEY (PRODUTO);
 
-DELETE FROM tabela_de_vendedores WHERE matricula='00233';
+SELECT * FROM tbproduto;
+
+INSERT INTO tbproduto (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) VALUES
+('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ml','Manga',5.18);
+
+UPDATE tbproduto SET EMBALAGEM = 'Garrafa'
+WHERE PRODUTO = '1078680';
